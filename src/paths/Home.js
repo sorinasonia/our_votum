e9d640b1678345628694ce2f687baa3ba7f7be7a
Blessing <bleble.erin@gmail.com~>
2020-08-30 15:44:13 +0100

    made changes

--- src/paths/Home.js
+++ src/paths/Home.js
@@ -1,19 +1,16 @@
 import React from "react";
 import './Home.css';
-import "../paths/Home.css";
 
 class Home extends React.Component{
-
 
     constructor(props){
       super(props)
     }
 
-
     text() {
-      return <div className = "random_text" >
-            <p> Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other
-              "longest texts ever" on the internet, and I wanted to make my own. So here it is! This
+      return <div className ="random_text" >
+            <p> Hello, everyone! This is the LONGEST TEXT EVER! I was inspired by the various other 
+                "longest texts ever" on the internet, and I wanted to make my own. So here it is! This
                 is going to be a WORLD RECORD! This is actually my third attempt at doing this. The first 
                 time, I didn't save it. The second time, the Neocities editor crashed. Now I'm writing this in
                 Notepad, then copying it into the Neocities editor instead of typing it directly in the Neocities 
@@ -21,20 +18,17 @@ class Home extends React.Component{
                 long. Not the longest, but still pretty long. I hope this one won't get lost somehow. Anyways, 
                 let's talk about WAFFLES! I like waffles. Waffles are cool. Waffles is a funny word. There's a Teen
                 Titans Go episode called "Waffles" where the word "Waffles" is said a hundred-something times. It's 
-                pretty annoying. There is also a Teen Titans Go episode about Pig Latin. Don't know what Pig Latin is? 
-                It's a language where you take all the consonants before the first vowel, move them to the end, and add 
-                '-ay' to the end. If the word begins with a vowel, you just add '-way' to the end. For example, "Waffles" 
-                becomes "Afflesway". I've been speaking Pig Latin fluently since the fourth grade, so it surprised me when 
-                I saw the episode for the first time. I speak Pig Latin with my sister sometimes. It's pretty fun. I like 
-                speaking it in public so that everyone around us gets confused.
-            </p>      
+            </p>   
+             
       </div>
     }
 
     render(){
       return (
         <div className = "home" >
-              <this.text/> 
+             
+                 <this.text/> 
+
         </div>
       );
     }
